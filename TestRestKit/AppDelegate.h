ed7c260bb61bbfaff5d2fb07ea3b16e6eedafdd4
Sergey <mukhinserg88@gmail.com>
2015-12-01 15:21:48 +0200

    Initial Commit

---
+++ TestRestKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestRestKit
+//
+//  Created by Sergey on 12/1/15.
+//  Copyright © 2015 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestRestKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestRestKit
+//
+//  Created by Sergey on 12/1/15.
+//  Copyright © 2015 Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
